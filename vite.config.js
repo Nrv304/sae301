25df388170e03a5b3f891ac94c4eae68c429a12b
Nrv304 <owentr04@gmail.com>
2025-10-24 13:36:41 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  base: "/Sae301/", // ← remplace par le nom exact de ton repo GitHub
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        account: resolve(__dirname, "pages/account.html"),
+        checkout: resolve(__dirname, "pages/checkout.html"),
+        enfant: resolve(__dirname, "pages/enfant.html"),
+        homme: resolve(__dirname, "pages/homme.html"),
+        femme: resolve(__dirname, "pages/femme.html"),
+        panier: resolve(__dirname, "pages/panier.html"),
+      },
+    },
+  },
 });
