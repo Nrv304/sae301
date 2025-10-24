0d80052bb8de880756997469f0750871e3ea3985
Nrv304 <owentr04@gmail.com>
2025-10-24 13:59:40 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Sae301/", // ← remplace par le nom exact de ton repo GitHub
+  base: "/sae301/", // ← remplace par le nom exact de ton repo GitHub
   build: {
     rollupOptions: {
       input: {
